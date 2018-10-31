972fda82d8d104dc338c4add2c7c6a32255af6d9
Astafiev Dmitry <astafievd@mail.ru>
2018-10-31 10:21:25 +0300

    Add ng-bootstrap

--- Quest/Web/ClientApp/src/app/app.module.ts
+++ Quest/Web/ClientApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule, XHRBackend } from '@angular/http';
 import { AuthenticateXHRBackend } from './authenticate-xhr.backend';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -14,18 +15,21 @@ import { routing } from './app.routing';
 
 import { ConfigService } from './shared/utils/config.service';
 import { AccountModule } from './account/account.module';
+import { QuestModal } from './home/questModal/quest-modal';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    QuestModal
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     HttpModule,
+    NgbModule,
     AccountModule,
     routing,
     UiModule,
---
+++ Quest/Web/ClientApp/src/app/home/questModal/quest-modal.js
@@ -0,0 +1,41 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@angular/core");
+const ng_bootstrap_1 = require("@ng-bootstrap/ng-bootstrap");
+let QuestModal = class QuestModal {
+    constructor(modalService) {
+        this.modalService = modalService;
+    }
+    open(content) {
+        this.modalService.open(content, { ariaLabelledBy: 'quest-modal-title' }).result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    }
+    getDismissReason(reason) {
+        if (reason === ng_bootstrap_1.ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        }
+        else if (reason === ng_bootstrap_1.ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        }
+        else {
+            return `with: ${reason}`;
+        }
+    }
+};
+QuestModal = __decorate([
+    core_1.Component({
+        selector: 'quest-modal',
+        templateUrl: './quest-modal.html'
+    })
+], QuestModal);
+exports.QuestModal = QuestModal;
+//# sourceMappingURL=quest-modal.js.map
